c111bb41269cb62878bb2dd83eb21f82f8908c7b
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-07-05 13:29:05 +0300

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word.downcase
+  end
+
+  def match(words)
+    words.select { |w| anagram?(w.downcase) }
+  end
+
+  private
+
+  def anagram?(other_word)
+    @word.chars.sort == other_word.chars.sort && @word != other_word
+  end
+end
